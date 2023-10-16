7e2c7356f3ca74b494b7ecb48c8111b08bce2b10
David Zechowy <davidzech27@gmail.com>
2023-10-15 23:39:47 -0700

    update twiml

--- app/laml/route.ts
+++ app/laml/route.ts
@@ -1,3 +1,27 @@
 export async function POST(request: Request) {
-	console.log(await request.text())
+	const body = decodeURIComponent(
+		(await request.text()).match(/(?<=&Body=).+(?=&)/g)?.[0] ?? ""
+	)
+
+	if (body === "STOP") {
+		return new Response(`<?xml version="1.0" encoding="UTF-8"?>
+<Response>
+    <Message><Body>You will no longer receive messages from us.</Body></Message>
+</Response>
+`)
+	} else if (body === "START") {
+		return new Response(`<?xml version="1.0" encoding="UTF-8"?>
+<Response>
+    <Message><Body>This is where you'll receive notifications from us. Reply with STOP to opt-out.</Body></Message>
+</Response>
+`)
+
+	} else if (body === "HELP") {
+		return new Response(`<?xml version="1.0" encoding="UTF-8"?>
+<Response>
+    <Message><Body>This is where you'll receive notifications from us. Reply with STOP to opt-out.</Body></Message>
+</Response>
+`)
+
+	}
 }

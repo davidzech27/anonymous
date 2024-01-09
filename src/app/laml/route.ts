4e9dc4e63fd9f778a2f60b5f304f17f4761bec7b
David Zechowy <davidzech27@gmail.com>
2024-01-08 18:52:54 -0800

    make sms laml compliant

--- src/app/laml/route.ts
+++ src/app/laml/route.ts
@@ -1,18 +1,39 @@
+import { eq } from "drizzle-orm"
+
+import db from "~/db/db"
+import { user } from "~/db/schema"
+
 export async function POST(request: Request) {
 	const text = await request.text()
-	console.log({ text })
+
 	const indexOfBody = text.indexOf("&Body=") + "&Body=".length
+
+	const phoneNumber = Number(text.match(/\d{7}(?=&To=)/)?.[0])
 
 	const body = decodeURIComponent(
 		text.slice(indexOfBody, text.indexOf("&", indexOfBody))
 	)
 
 	if (body === "STOP") {
+		await db
+			.update(user)
+			.set({
+				smsNotificationConsent: false,
+			})
+			.where(eq(user.phoneNumber, phoneNumber))
+
 		return new Response(`<?xml version="1.0" encoding="UTF-8"?>
 <Response>
     <Message><Body>You've unsubscribed from messages from mchsanonymous. Reply with START to resubscribe to notifications.</Body></Message>
 </Response>`)
 	} else if (body === "START") {
+		await db
+			.update(user)
+			.set({
+				smsNotificationConsent: true,
+			})
+			.where(eq(user.phoneNumber, phoneNumber))
+
 		return new Response(`<?xml version="1.0" encoding="UTF-8"?>
 <Response>
     <Message><Body>You've subscribed to notifications from mchsanonymous. Notifications indicating the number of unread messages you have, if any, are sent at most once per day. Reply with STOP to opt-out. Message frequency depends on activity and Msg&Data rates may apply.</Body></Message>

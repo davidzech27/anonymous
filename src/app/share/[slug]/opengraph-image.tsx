defbaac8c8c4c4263dabbefbd8323c5d980070bd
David Zechowy <davidzech27@gmail.com>
2024-01-09 20:45:22 -0800

    test

--- src/app/share/[slug]/opengraph-image.tsx
+++ src/app/share/[slug]/opengraph-image.tsx
@@ -8,75 +8,75 @@ import { conversation, message, user } from "~/db/schema"
 
 export const runtime = "edge"
 
-export default async function SharePreview({
+export default function SharePreview({
 	params,
 }: {
 	params: Record<string, unknown>
 }) {
-	const { slug } = params
+	// const { slug } = params
 
-	if (typeof slug !== "string") notFound()
+	// if (typeof slug !== "string") notFound()
 
-	const decodedSlug = decodeURIComponent(slug)
+	// const decodedSlug = decodeURIComponent(slug)
 
-	const userId = Number(decodedSlug.split(",")[0])
+	// const userId = Number(decodedSlug.split(",")[0])
 
-	const conversationId = Number(decodedSlug.split(",")[1])
+	// const conversationId = Number(decodedSlug.split(",")[1])
 
-	const rangeString = decodedSlug.split(",")[2]
+	// const rangeString = decodedSlug.split(",")[2]
 
-	const startIndex = Number(rangeString?.split("-")[0])
+	// const startIndex = Number(rangeString?.split("-")[0])
 
-	const endIndex = Number(rangeString?.split("-")[1])
+	// const endIndex = Number(rangeString?.split("-")[1])
 
-	if (
-		isNaN(userId) ||
-		isNaN(conversationId) ||
-		isNaN(startIndex) ||
-		isNaN(endIndex)
-	) {
-		notFound()
-	}
+	// if (
+	// 	isNaN(userId) ||
+	// 	isNaN(conversationId) ||
+	// 	isNaN(startIndex) ||
+	// 	isNaN(endIndex)
+	// ) {
+	// 	notFound()
+	// }
 
-	const [[userRow], [conversationRow], messageRows] = await Promise.all([
-		db.select().from(user).where(eq(user.id, userId)),
-		db
-			.select()
-			.from(conversation)
-			.where(eq(conversation.id, conversationId)),
-		db
-			.select()
-			.from(message)
-			.where(eq(message.conversationId, conversationId)),
-	])
+	// const [[userRow], [conversationRow], messageRows] = await Promise.all([
+	// 	db.select().from(user).where(eq(user.id, userId)),
+	// 	db
+	// 		.select()
+	// 		.from(conversation)
+	// 		.where(eq(conversation.id, conversationId)),
+	// 	db
+	// 		.select()
+	// 		.from(message)
+	// 		.where(eq(message.conversationId, conversationId)),
+	// ])
 
-	if (userRow === undefined || conversationRow === undefined) {
-		notFound()
-	}
+	// if (userRow === undefined || conversationRow === undefined) {
+	// 	notFound()
+	// }
 
-	if (
-		userId !== conversationRow.anonymousUserId &&
-		userId !== conversationRow.knownUserId
-	) {
-		notFound()
-	}
+	// if (
+	// 	userId !== conversationRow.anonymousUserId &&
+	// 	userId !== conversationRow.knownUserId
+	// ) {
+	// 	notFound()
+	// }
 
-	const messages = messageRows.sort(
-		(message1, message2) =>
-			message1.sentAt.getTime() - message2.sentAt.getTime()
-	)
+	// const messages = messageRows.sort(
+	// 	(message1, message2) =>
+	// 		message1.sentAt.getTime() - message2.sentAt.getTime()
+	// )
 
-	const selectedMessages = messages.slice(startIndex - 1, endIndex)
+	// const selectedMessages = messages.slice(startIndex - 1, endIndex)
 
-	if (selectedMessages.length === 0) {
-		notFound()
-	}
+	// if (selectedMessages.length === 0) {
+	// 	notFound()
+	// }
 
 	return new ImageResponse(
 		(
 			<div className="flex h-full flex-col space-y-3 bg-primary p-6 mobile:p-5">
 				<div className="flex-1 space-y-3 overflow-y-auto rounded-lg border border-white p-3">
-					{selectedMessages.map((message) => (
+					{/* {selectedMessages.map((message) => (
 						<Fragment key={message.id}>
 							<div className="flex flex-col space-y-3 rounded-lg border border-white bg-white/20 p-3">
 								<div className="text-lg font-bold leading-none text-secondary">
@@ -97,7 +97,7 @@ export default async function SharePreview({
 								)}
 							</div>
 						</Fragment>
-					))}
+					))} */}
 				</div>
 			</div>
 		)
